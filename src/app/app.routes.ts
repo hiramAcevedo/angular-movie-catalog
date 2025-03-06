60612610b2b8949a80cc3829ec3ee3769ff377dc
hiramAcevedo <hiram.acevedo.lopez@gmail.com>
2025-03-06 12:15:21 -0700

    Notas sobre rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { MovieListComponent } from './components/movie-list/movie-list.component
 import { MovieDetailComponent } from './components/movie-detail/movie-detail.component';
 
 export const routes: Routes = [
-    { path: '', component: MovieListComponent },
-    { path: 'movie/:id', component: MovieDetailComponent },
+    { path: '', component: MovieListComponent }, // Ruta principal: Catálogo
+    { path: 'movie/:id', component: MovieDetailComponent }, // Ruta dinámica: Detalles de película
     { path: '**', redirectTo: '' }
 ];
